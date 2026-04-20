1d213f879ad3dc6c714277956790cfdbb0816db3
gusafig <gusafig@users.noreply.github.com>
2026-04-20 17:37:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
