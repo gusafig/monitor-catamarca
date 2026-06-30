b63815508172856dc703b6174f56b20e239fd7ed
gusafig <gusafig@users.noreply.github.com>
2026-06-30 08:26:38 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  publicDir: "public",
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 3000,
+  },
+});
